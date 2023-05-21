fd03d817fc9cf616634e993d399318cc5c6d38d1
orwenn22 <46846090+orwenn22@users.noreply.github.com>
2023-05-21 17:51:14 +0200

    oops

---
+++ src/BaseUI/Generic/WinWidgetContainer.cpp
@@ -0,0 +1,36 @@
+#include "WinWidgetContainer.h"
+
+#include "../../Core/RunState.h"
+#include "../../Globals.h"
+
+WinWidgetContainer::WinWidgetContainer() : WidgetContainer() {
+
+}
+
+void WinWidgetContainer::Update() {
+    UpdateWidgets();
+}
+
+void WinWidgetContainer::Render() {
+    RenderWidgets();
+}
+
+void WinWidgetContainer::GetAbsoluteSize(int* w, int* h) {
+    *w = GetAbsoluteWidth();
+    *h = GetAbsoluteHeight();
+}
+int WinWidgetContainer::GetAbsoluteWidth() {
+    return g_runstate->winwidth;
+}
+int WinWidgetContainer::GetAbsoluteHeight() {
+    return g_runstate->winheight;
+}
+
+bool WinWidgetContainer::IsMouseOvering() {
+    return true;    //from our perspective, assume the mouse is always overring the window
+}
+
+void WinWidgetContainer::GetRelativeMousePos(int* x, int* y) {
+    *x = g_runstate->mousex;
+    *y = g_runstate->mousey;
+}
