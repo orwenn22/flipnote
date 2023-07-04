1660cf6920287551e7f8ba6589bfba6fa61bc8d6
orwenn22 <46846090+orwenn22@users.noreply.github.com>
2023-07-04 14:13:57 +0200

    Remove unused Utils functions

--- src/Core/Utils.h
+++ src/Core/Utils.h
@@ -5,13 +5,6 @@
 
 Uint32 SDLColorToUint32(SDL_Color c);
 
-void SurfaceSetPixel(SDL_Surface* surface, int x, int y, Uint32 col);
-
-void SurfaceDrawLine(SDL_Surface *surface, int x0, int y0, int x1, int y1, Uint32 col);
-void SurfaceDrawLineEx(SDL_Surface *surface, int x0, int y0, int x1, int y1, int thickness, Uint32 col);
-
-void SurfaceDrawCircle(SDL_Surface *surface, int x, int y, int r, Uint32 col);
-void SurfaceFillCircle(SDL_Surface *surface, int x, int y, int r, Uint32 col);
 
 
 #endif
