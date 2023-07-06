ef6128ded082c6ee6f822bb2e926bf0b79f49dd4
orwenn22 <46846090+orwenn22@users.noreply.github.com>
2023-07-06 18:18:54 +0200

    Remove useless spaces in the DebugOverlay

--- src/Reusable/DebugOverlay.cpp
+++ src/Reusable/DebugOverlay.cpp
@@ -8,8 +8,8 @@
 #include "RunState.h"
 
 void RenderDebugOverlay(int basex, int basey) {
-    RenderText((std::string("Renderer  : ") + g_runstate->rendererinfo->name).c_str()       , basex, basey, g_reusableressources->font_ubuntumedium16, {0, 255, 0, 255}, {0, 0, 0, 255});
+    RenderText((std::string("Renderer : ")  + g_runstate->rendererinfo->name).c_str()       , basex, basey, g_reusableressources->font_ubuntumedium16, {0, 255, 0, 255}, {0, 0, 0, 255});
     RenderText((std::string("DeltaTime : ") + std::to_string(g_deltatime)).c_str()          , basex, basey+18, g_reusableressources->font_ubuntumedium16, {0, 255, 0, 255}, {0, 0, 0, 255});
     RenderText((std::string("FrameTime : ") + std::to_string(GetCurrentFrameTime())).c_str(), basex, basey+36, g_reusableressources->font_ubuntumedium16, {0, 255, 0, 255}, {0, 0, 0, 255});
-    RenderText((std::string("FPS       : ") + std::to_string(1.0/g_deltatime)).c_str()       , basex, basey+54, g_reusableressources->font_ubuntumedium16, {0, 255, 0, 255}, {0, 0, 0, 255});
+    RenderText((std::string("FPS : ")       + std::to_string(1.0/g_deltatime)).c_str()       , basex, basey+54, g_reusableressources->font_ubuntumedium16, {0, 255, 0, 255}, {0, 0, 0, 255});
 }
